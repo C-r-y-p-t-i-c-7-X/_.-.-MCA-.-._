e6dd252cdd0d4c3215836e31beb95a7551d66018
Madhab Mohan Behera <112565113+C-r-y-p-t-i-c-7-X@users.noreply.github.com>
2026-08-19 21:47:42 +0530

    Create #Q7.c

---
+++ 1st sem/Assignment-1/#Q7.c
@@ -0,0 +1,21 @@
+// 7.Input Temperature in Celsius and convert it into Fahrenheit and vice versa.
+
+#include <stdio.h>
+
+int main() {
+    float c, f, fahrenheit, celsius;
+
+    // 1. Celsius to Fahrenheit
+    printf("Enter Temperature in Celsius: ");
+    scanf("%f", &c);
+    fahrenheit = (c * 9.0 / 5.0) + 32.0;
+    printf("Temperature in Fahrenheit = %.2f\n\n", fahrenheit);
+
+    // 2. Fahrenheit to Celsius
+    printf("Enter Temperature in Fahrenheit: ");
+    scanf("%f", &f);
+    celsius = (f - 32.0) * 5.0 / 9.0;
+    printf("Temperature in Celsius = %.2f\n", celsius);
+
+    return 0;
+}
