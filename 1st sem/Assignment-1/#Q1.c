118a0ee999b0dfd9d36f90fff92d214feef6bee2
Madhab Mohan Behera <112565113+C-r-y-p-t-i-c-7-X@users.noreply.github.com>
2026-08-19 20:06:19 +0530

    Implement student details input and display program
    This program takes user input for name, age, roll number, branch, and percentage, then displays the details.

---
+++ 1st sem/Assignment-1/#Q1.c
@@ -0,0 +1,37 @@
+//
+//Q1- Input your name, age, roll_no branch, percentage of marks and display.
+
+#include <stdio.h>
+
+int main() {
+    char name[50], branch[50];
+    int age, roll_no;
+    float percentage;
+
+    printf("Enter Name: ");
+    
+    scanf(" %[^\n]", name);
+//    %[^\n] is the scanset conversion specifier in scanf dfference from %s and %c
+//	%s read until the space and %c reads only 1 byte charater.
+    printf("Enter Age: ");
+    scanf("%d", &age);
+    printf("Enter Roll No: ");
+    scanf("%d", &roll_no);
+    printf("Enter Branch: ");
+    scanf(" %[^\n]", branch);
+    printf("Enter Percentage of Marks: ");
+    scanf("%f", &percentage);
+
+    printf("\n--- Student Details ---\n");
+    printf("Name       : %s\n", name);
+    printf("Age        : %d\n", age);
+    printf("Roll No    : %d\n", roll_no);
+    printf("Branch     : %s\n", branch);
+    printf("Percentage : %.2f%%\n", percentage);
+    
+//    %.2f%% " is %2f is formart specifies and .2 is a specifies that after decimal only 2 digit to show. "%%" as single % can't printed directly so we use %% it will print in outupt.
+
+    return 0;
+}
+
+
