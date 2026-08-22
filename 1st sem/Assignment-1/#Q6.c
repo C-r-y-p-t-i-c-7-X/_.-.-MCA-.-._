71dc58260135939f322a1f64b62dc2515a8dcbf3
Madhab Mohan Behera <112565113+C-r-y-p-t-i-c-7-X@users.noreply.github.com>
2026-08-22 14:42:07 +0530

    Update #Q6.c

--- 1st sem/Assignment-1/#Q6.c
+++ 1st sem/Assignment-1/#Q6.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 int main() {
+
     char name[50], branch[20];
     int roll_no, sem;
     float m1, m2, m3, m4, m5, total, percentage;
@@ -10,16 +11,34 @@ int main() {
     // Input student details
     printf("Enter Name: ");
     scanf("%s", name);
+
     printf("Enter Roll No: ");
     scanf("%d", &roll_no);
+
     printf("Enter Branch: ");
     scanf("%s", branch);
+
     printf("Enter Semester: ");
     scanf("%d", &sem);
 
     // Input marks for 5 subjects
-    printf("Enter marks for 5 subjects: ");
-    scanf("%f %f %f %f %f", &m1, &m2, &m3, &m4, &m5);
+    
+    printf("Enter marks for 5 subjects:\n");
+
+    printf("Subject 1: ");
+    scanf("%f", &m1);
+
+    printf("Subject 2: ");
+    scanf("%f", &m2);
+
+    printf("Subject 3: ");
+    scanf("%f", &m3);
+
+    printf("Subject 4: ");
+    scanf("%f", &m4);
+
+    printf("Subject 5: ");
+    scanf("%f", &m5);
 
     // Calculations
     total = m1 + m2 + m3 + m4 + m5;
@@ -36,3 +55,28 @@ int main() {
 
     return 0;
 }
+
+/*
+Output:-
+--------------------
+
+Enter Name: Madhav
+Enter Roll No: 111
+Enter Branch: MCA
+Enter Semester: 1
+Enter marks for 5 subjects:
+Subject 1: 74
+Subject 2: 79
+Subject 3: 82
+Subject 4: 89
+Subject 5: 93
+
+----- MARK SHEET -----
+Name        : Madhav
+Roll No     : 111
+Branch      : MCA
+Semester    : 1
+Total Marks : 417.00 / 500
+Percentage  : 83.40%
+
+    */
